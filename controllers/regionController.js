f4d107444d715dd21f4d6ad43fb1b5c6d4c59e59
Chloe Brown <chloemail@protonmail.com>
2022-05-11 14:24:44 +0100

    area total and highest country area

--- controllers/regionController.js
+++ controllers/regionController.js
@@ -16,20 +16,37 @@ export default async function ultimateResponse(req, res) {
     countriesArray = data.map(country => {
       return {
         countryName : country.name.common,
-        population : country.population
+        population : country.population,
+        area: country.area
       }
     })
-    
+    console.log('COUNTRIES ARRAY -->', countriesArray)
+
     // get the total population of all the country populations
-    const totalPopulation = countriesArray.reduce(function (acc, obj) {
+    const totalPopulation = countriesArray.reduce((acc, obj) => {
       return acc + obj.population
     }, 0)
     
-    const highestPopulation = countriesArray.reduce((prev, current) => (+prev.population > +current.population) ? prev : current)
+    const highestPopulation = countriesArray.reduce((prev, current) => (prev.population > current.population) ? prev : { countryName: current.countryName, population: current.population})
+    
+    // 1. return the total area of the region
+    // 2. return the country with the highest area
+
+    const totalArea = countriesArray.reduce((acc, obj) => {
+      return acc + obj.area
+    }, 0)
+    console.log('TOTAL AREA -->', totalArea)
+
+    const highestArea = countriesArray.reduce((prev, current) => (prev.area > current.area) ? prev : { countryName: current.countryName, area: current.area})
+    console.log('HIGHEST AREA -->', highestArea)
 
     // store the data in the response
     response.regionTotalPopulation = totalPopulation
     response.highestPopulationCountry = highestPopulation
+    response.totalArea = totalArea
+    response.highestAreaCountry = highestArea
+
+    console.log('FINAL RESPONSE -->', response)
 
     // Return to user
     return res.status(200).json(response)
